3912085f8023acf66504ea09c414186440b1acf6
kow <kow@em70.ru>
2019-05-24 02:13:30 +0700

    Tmp

--- MessageServer/Global/AsynchronousServer.cs
+++ MessageServer/Global/AsynchronousServer.cs
@@ -37,16 +37,17 @@ namespace MessageServer
         public async void Start()
         {
             TcpListener listener = new TcpListener(this.ipAddress, this.port);
-            listener.Start();
-            
+            listener.Start();  
 
             while (true)
             {
                 try
                 { 
                     TcpClient tcpClient = await listener.AcceptTcpClientAsync();
-                    //Task t = Process(tcpClient);
-                    //await t;
+
+                    Task processTask = Process(tcpClient);
+                    await processTask;
+
                     logger.Info("Server started");
                 }
                 catch (Exception ex)
@@ -55,5 +56,68 @@ namespace MessageServer
                 }
             }
         }
+
+        private async Task Process(TcpClient tcpClient)
+        {
+            string clientEndPoint = tcpClient.Client.RemoteEndPoint.ToString();
+
+            try
+            {
+                NetworkStream networkStream = tcpClient.GetStream();
+                StreamReader reader = new StreamReader(networkStream);
+                StreamWriter writer = new StreamWriter(networkStream);
+                writer.AutoFlush = true;
+
+                while (true)
+                {
+                    string request = await reader.ReadLineAsync();
+                    if (request != null)
+                    {
+                        /*Console.WriteLine("Received service request: " + request);
+                        string response = Response(request);
+                        Console.WriteLine("Computed response is: " + response + "\n");
+                        await writer.WriteLineAsync(response);*/
+                    }
+                    else
+                        break; // client closede connection
+                }
+                tcpClient.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                if (tcpClient.Connected)
+                    tcpClient.Close();
+            }
+        }
+
+        private string Response(string request)
+        {
+            // assumes request has form like method=average&data=1.1 2.2 3.3&eor
+            // eor stands for end-of-request
+            // dummy delay bssed on the first numeric value
+            /*string[] pairs = request.Split('&');
+            string methodName = pairs[0].Split('=')[1];
+            string valueString = pairs[1].Split('=')[1];
+
+            string[] values = valueString.Split(' ');
+            double[] vals = new double[values.Length];
+            for (int i = 0; i < values.Length; ++i)
+                vals[i] = double.Parse(values[i]);
+
+            string response = "";
+            if (methodName == "average") response += Average(vals);
+            else if (methodName == "minimum") response += Minimum(vals);
+            else response += "BAD methodName: " + methodName;
+
+            int delay = ((int)vals[0]) * 1000; // dummy delay
+            System.Threading.Thread.Sleep(delay);
+
+            return response;*/
+
+            return "";
+        }
+
+
     }
 }
---
+++ MessageServer/ViewModels/ServerViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net; //TODO: delete
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageServer.ViewModels
+{
+    public class ServerViewModel
+    {
+        public ServerViewModel()
+        {
+            string hostName = Dns.GetHostName();
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(hostName);
+
+            List<String> values = new List<String>();
+            foreach (NetworkInterface adapter in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (adapter.NetworkInterfaceType == NetworkInterfaceType.Ethernet ||
+                    adapter.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
+                {
+                    foreach (UnicastIPAddressInformation ip in adapter.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            Console.WriteLine(ip.Address.ToString());
+                            values.Add(adapter.Description);
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < ipHostInfo.AddressList.Length; ++i)
+            {
+                if (ipHostInfo.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                {
+                    //this.ipAddress = ipHostInfo.AddressList[i];
+                    break;
+                }
+            }
+        }
+    }
+}
